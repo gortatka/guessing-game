6b2bb5b7038f5f434afa23d394547da1c2458abf
gortatka <gtatsiana@tut.by>
2021-09-06 21:31:20 +0300

    feat: add functions

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this._guess = Math.ceil((this.max - this.min) / 2) + this.min;
+        return this._guess;
     }
 
     lower() {
-
+        this.setRange(this.min, this._guess);
+        this.guess();
     }
 
     greater() {
-
+        this.setRange(this._guess, this.max);
+        this.guess();
     }
 }
 
